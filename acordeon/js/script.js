a5a727775c0ceb49bca5bc55bebef6ff89af735a
Gorpetrosov <gorpetrosyan1991@gmail.com>
2017-11-28 22:44:51 +0400

    acardion menu

---
+++ acordeon/js/script.js
@@ -0,0 +1,11 @@
+function myDrop1() {
+    document.getElementById('myBlock1').classList.toggle('show');
+}
+
+function myDrop2() {
+    document.getElementById('myBlock2').classList.toggle('show');
+}
+
+function myDrop3() {
+    document.getElementById('myBlock3').classList.toggle('show');
+}
