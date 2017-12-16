3a6ede9853f67a38b31e106acb118a7ac3db9ec8
Gorpetrosov <gorpetrosyan1991@gmail.com>
2017-12-17 01:09:20 +0400

    SalvatoreAnimatedPhoto

---
+++ Salvatore/js/sal.js
@@ -0,0 +1,5 @@
+// var grid = document.querySelector('#grid');
+// var item = document.createElement('article');
+//
+// salvattore.appendElements(grid, [item]);
+// item.outerHTML = 'I’ve been appended!';
